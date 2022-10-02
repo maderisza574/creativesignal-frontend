6a2c3b391afe1b0bdddb5b6a67a2954378f39b88
maderisza <maderisza@gmail.com>
2022-10-02 22:42:14 +0700

    add some detail footer

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -83,124 +83,93 @@ function Signin() {
           </div>
         </div>
       </div>
-      <footer className="text-center text-lg-start bg-light text-muted">
-        {/* social media section */}
-        <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-          {/* left */}
-          <div className="me-5 d-none d-lg-block"></div>
-          {/* end left */}
-          {/* right */}
-          <div>
-            <a href="" className="me-4 text-reset">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="" className="me-4 text-reset">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href="" className="me-4 text-reset">
-              <i className="fab fa-google"></i>
-            </a>
-            <a href="" className="me-4 text-reset">
-              <i className="fab fa-instagram"></i>
-            </a>
-            <a href="" className="me-4 text-reset">
-              <i className="fab fa-linkedin"></i>
-            </a>
-            <a href="" className="me-4 text-reset">
-              <i className="fab fa-github"></i>
-            </a>
+      <footer className="footer container-fluid text-center text-md-start mt-5">
+        <div className="row mt-3">
+          <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <img src={logo} className="img-fluid" alt="logo"></img>
+            <p>Find events you love with our</p>
           </div>
-          {/* end right */}
-        </section>
-        <section className="">
-          <div className="container text-center text-md-start mt-5">
-            <div className="row mt-3">
-              <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                <img src={logo} className="img-fluid" alt="logo"></img>
-                <p>Find events you love with our</p>
-              </div>
-              <div className="col-md-2 col-lg-2 col-xl-2 px-0 mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">Wetick</h6>
-                <p>
-                  <a href="#!" className="text-reset">
-                    About Us
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Features
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Blog
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Payments
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Mobile App
-                  </a>
-                </p>
-              </div>
-              <div className="col-md-3 col-lg-2 col-xl-2 px-0 mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">Features</h6>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Booking
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Create Event
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Discover
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Register
-                  </a>
-                </p>
-              </div>
-              <div className="col-md-2 col-lg-2 col-xl-2 px-0 mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">Company</h6>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Partnership
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Help
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Terms of Service
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Privacy Policy
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Sitemap
-                  </a>
-                </p>
-              </div>
-            </div>
+          <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <h6 className="text-uppercase fw-bold mb-4">Wetick</h6>
+            <p>
+              <a href="#!" className="text-reset">
+                About Us
+              </a>
+            </p>
+            <p>
+              <a href="#!" className="text-reset">
+                Features
+              </a>
+            </p>
+            <p>
+              <a href="#!" className="text-reset">
+                Blog
+              </a>
+            </p>
+            <p>
+              <a href="#!" className="text-reset">
+                Payments
+              </a>
+            </p>
+            <p>
+              <a href="#!" className="text-reset">
+                Mobile App
+              </a>
+            </p>
           </div>
-        </section>
+          <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <h6 className="text-uppercase fw-bold mb-4">Features</h6>
+            <p>
+              <a href="#!" className="text-reset">
+                Booking
+              </a>
+            </p>
+            <p>
+              <a href="#!" className="text-reset">
+                Create Event
+              </a>
+            </p>
+            <p>
+              <a href="#!" className="text-reset">
+                Discover
+              </a>
+            </p>
+            <p>
+              <a href="#!" className="text-reset">
+                Register
+              </a>
+            </p>
+          </div>
+          <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <h6 className="text-uppercase fw-bold mb-4">Company</h6>
+            <p>
+              <a href="#!" className="text-reset">
+                Partnership
+              </a>
+            </p>
+            <p>
+              <a href="#!" className="text-reset">
+                Help
+              </a>
+            </p>
+            <p>
+              <a href="#!" className="text-reset">
+                Terms of Service
+              </a>
+            </p>
+            <p>
+              <a href="#!" className="text-reset">
+                Privacy Policy
+              </a>
+            </p>
+            <p>
+              <a href="#!" className="text-reset">
+                Sitemap
+              </a>
+            </p>
+          </div>
+          <div className="text-left p-4">© 2022 Copyright:</div>
+        </div>
       </footer>
     </>
   );
